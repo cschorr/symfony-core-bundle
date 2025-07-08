081103bdb77d1220f11a0d2376195328234f5d62
Philip Fueckert <me@pfpro.de>
2025-07-08 21:11:05 +0200

    task: add kickstart.sh

---
+++ bin/kickstart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ddev php bin/console doctrine:database:drop --force
+ddev php bin/console doctrine:database:create
+ddev php bin/console doctrine:schema:update --force
+#ddev php bin/console doctrine:fixtures:load --no-interaction
+
+#ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
+#ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
