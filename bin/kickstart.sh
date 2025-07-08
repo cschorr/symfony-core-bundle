ccb0198431b245109b660b71b0350074e4b2552c
Philip Fueckert <me@pfpro.de>
2025-07-08 21:21:55 +0200

    fix: enable fixtures

--- bin/kickstart.sh
+++ bin/kickstart.sh
@@ -3,7 +3,7 @@
 ddev php bin/console doctrine:database:drop --force
 ddev php bin/console doctrine:database:create
 ddev php bin/console doctrine:schema:update --force
-#ddev php bin/console doctrine:fixtures:load --no-interaction
+ddev php bin/console doctrine:fixtures:load --no-interaction
 
-#ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
-#ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
+ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
+ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
