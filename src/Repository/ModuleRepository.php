d9bce0f1f3ca653ed86e76b034481e0780d01efb
Philip Fueckert <me@pfpro.de>
2025-07-08 22:36:19 +0200

    fix: module

--- src/Repository/ModuleRepository.php
+++ src/Repository/ModuleRepository.php
@@ -2,14 +2,14 @@
 
 namespace App\Repository;
 
-use App\Entity\Company;
+use App\Entity\Module;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Module>
  */
-class ModuleRepositoryRepository extends ServiceEntityRepository
+class ModuleRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
