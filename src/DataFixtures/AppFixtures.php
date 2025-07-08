0e89f2cdc6db2f7ab2a0ae38abbf8b98288b7178
Philip Fueckert <me@pfpro.de>
2025-07-08 21:28:55 +0200

    fix: hashing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,14 +6,22 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user->setEmail('admin@example.com');
-        $user->setPassword('pass_1234');
+        $user->setPassword($this->hasher->hashPassword($user, 'pass_1234'));
         $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
