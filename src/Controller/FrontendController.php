21c45b7b12281f7bcfdb33a311bc26c232238ea7
Philip Fueckert <me@pfpro.de>
2025-07-08 21:15:47 +0200

    task: add redirect to login

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -8,11 +8,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class FrontendController extends AbstractController
 {
-    #[Route('/frontend', name: 'app_frontend')]
+    #[Route('/', name: 'app_frontend')]
     public function index(): Response
     {
-        return $this->render('frontend/index.html.twig', [
-            'controller_name' => 'FrontendController',
-        ]);
+        // redirect
+        return $this->redirectToRoute('app_login');
     }
 }
