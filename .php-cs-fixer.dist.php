da059fa08a96eadcfa223699501b2b4818559532
Christoph Schorr <c.schorr@c3net.de>
2025-07-23 16:33:09 +0200

    feat: add code beautifier script

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,6 +10,10 @@ return (new PhpCsFixer\Config())
         '@Symfony' => true,
         'declare_strict_types' => true,
         'concat_space' => false,
+        'strict_comparison' => true,
+        'strict_param' => true,
+
     ])
+    ->setRiskyAllowed(true)
     ->setFinder($finder)
 ;
--- bin/kickstart.sh
+++ bin/kickstart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ddev php bin/console doctrine:database:drop --force
-ddev php bin/console doctrine:database:create
-ddev php bin/console doctrine:schema:update --force
-ddev php bin/console doctrine:fixtures:load --no-interaction
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load --no-interaction
 
-ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
-ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
+php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
+php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
