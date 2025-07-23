c9b9a93a16557b32bea6075a6406a861b0d35bfe
Christoph Schorr <c.schorr@c3net.de>
2025-07-23 14:55:35 +0200

    fix: remove enum property from project entity to re-enable diagram

--- bin/kickstart.sh
+++ bin/kickstart.sh
@@ -5,5 +5,5 @@ ddev php bin/console doctrine:database:create
 ddev php bin/console doctrine:schema:update --force
 ddev php bin/console doctrine:fixtures:load --no-interaction
 
-#ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
-#ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
+ddev php bin/console doctrine:diagram:class --filename=assets/docs/diagram-class.svg
+ddev php bin/console doctrine:diagram:er --filename=assets/docs/diagram-er.svg
