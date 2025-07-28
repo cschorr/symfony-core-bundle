bba7795f9f7c27a0e224d1ee0784c82db6d30f7e
Christoph Schorr <c.schorr@c3net.de>
2025-07-28 21:50:18 +0200

    feat: add usergroup entity and change system entity permissions from user to usergroup

--- src/Entity/UserGroupSystemEntityPermission.php
+++ src/Entity/UserGroupSystemEntityPermission.php
@@ -8,15 +8,15 @@ use App\Repository\UserSystemEntityPermissionRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserSystemEntityPermissionRepository::class)]
-#[ORM\Table(name: 'user_system_entity_permission')]
-#[ORM\UniqueConstraint(name: 'UNIQ_USER_SYSTEM_ENTITY', fields: ['user', 'systemEntity'])]
-class UserSystemEntityPermission extends AbstractEntity
+#[ORM\Table(name: 'usergroup_system_entity_permission')]
+#[ORM\UniqueConstraint(name: 'UNIQ_USER_SYSTEM_ENTITY', fields: ['userGroup', 'systemEntity'])]
+class UserGroupSystemEntityPermission extends AbstractEntity
 {
-    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'systemEntityPermissions')]
-    #[ORM\JoinColumn(name: 'user_id', nullable: false)]
-    private ?User $user = null;
+    #[ORM\ManyToOne(targetEntity: UserGroup::class, inversedBy: 'systemEntityPermissions')]
+    #[ORM\JoinColumn(name: 'usergroup_id', nullable: false)]
+    private ?UserGroup $userGroup = null;
 
-    #[ORM\ManyToOne(targetEntity: SystemEntity::class, inversedBy: 'userPermissions')]
+    #[ORM\ManyToOne(targetEntity: SystemEntity::class, inversedBy: 'userGroupPermissions')]
     #[ORM\JoinColumn(name: 'system_entity_id', nullable: false)]
     private ?SystemEntity $systemEntity = null;
 
@@ -38,21 +38,21 @@ class UserSystemEntityPermission extends AbstractEntity
     {
         return sprintf(
             '%s - %s (R:%s, W:%s)',
-            $this->user?->getEmail() ?? 'Unknown User',
+            $this->userGroup?->getEmail() ?? 'Unknown User',
             $this->systemEntity?->getName() ?? 'Unknown SystemEntity',
             $this->canRead ? 'Y' : 'N',
             $this->canWrite ? 'Y' : 'N'
         );
     }
 
-    public function getUser(): ?User
+    public function getUserGroup(): ?UserGroup
     {
-        return $this->user;
+        return $this->userGroup;
     }
 
-    public function setUser(?User $user): static
+    public function setUserGroup(?UserGroup $userGroup): static
     {
-        $this->user = $user;
+        $this->userGroup = $userGroup;
 
         return $this;
     }
