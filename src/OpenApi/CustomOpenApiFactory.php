20faf54b8dbb7a453808a9555ffad713dd3c78cb
Christoph Schorr <c.schorr@c3net.de>
2025-08-08 10:50:19 +0200

    fix: API authentication & improve openapi documentation

---
+++ src/OpenApi/CustomOpenApiFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\OpenApi;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\Model\Server;
+use ApiPlatform\OpenApi\OpenApi;
+
+final class CustomOpenApiFactory implements OpenApiFactoryInterface
+{
+    public function __construct(private OpenApiFactoryInterface $decorated) {}
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = ($this->decorated)($context);
+
+        // keep any existing servers and add yours
+        $servers = $openApi->getServers() ?? [];
+        $servers[] = new Server('https://api.example.com', 'Production API');
+        $servers[] = new Server('https://globe.c3net.io', 'Staging API');
+
+        return $openApi->withServers($servers);
+    }
+}
