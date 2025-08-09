fe3447d08898aad4599f09a082b3cfb2cf398959
Christoph Schorr <c.schorr@c3net.de>
2025-08-09 10:56:59 +0200

    doc: add endpoint /token/refresh to openapi documentation

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\Metadata\Post;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
 use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
 
 #[Entity]
 #[Table(name: "refresh_tokens")]
+#[ApiResource(
+    operations: [
+        new Post(
+            openapi: new Operation(
+                tags: ['Login Check']
+            )
+        )
+    ]
+)]
 class RefreshToken extends BaseRefreshToken
 {
 }
