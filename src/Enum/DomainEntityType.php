42565f865783f8d37d2875faa172ee1693686b7a
Christoph Schorr <c.schorr@c3net.de>
2025-10-05 17:09:40 +0200

    feat: add Campaign and Contact to DomainEntityType enum
    - Add Campaign case (alphabetically sorted)
    - Contact case was already present
    - Prepare for category polymorphic relationships

--- src/Enum/DomainEntityType.php
+++ src/Enum/DomainEntityType.php
@@ -6,18 +6,19 @@ namespace C3net\CoreBundle\Enum;
 
 enum DomainEntityType: string
 {
+    case Campaign = 'campaign';
     case Category = 'category';
     case Company = 'company';
     case CompanyGroup = 'company_group';
     case Contact = 'contact';
+    case Document = 'document';
+    case Invoice = 'invoice';
     case Notification = 'notification';
+    case Offer = 'offer';
     case Project = 'project';
     case ProjectStatus = 'project_status';
     case Thread = 'thread';
+    case Transaction = 'transaction';
     case User = 'user';
     case UserGroup = 'user_group';
-    case Transaction = 'transaction';
-    case Offer = 'offer';
-    case Invoice = 'invoice';
-    case Document = 'document';
 }
